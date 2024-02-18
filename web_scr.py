052833f6118d26c9fb0ff94e65fb7a3772477c73
aleksandra0014 <a.walczybok4@gmail.com>
2024-02-18 20:50:18 +0100

    Wstępna wersja

--- web_scr.py
+++ web_scr.py
@@ -3,6 +3,7 @@ import requests
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class Car:
     brand: str
@@ -21,7 +22,7 @@ class Car:
 
 
 class Scraper:
-    def __init__(self, car_make: str) -> None:
+    def __init__(self) -> None:
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/88.0.4324.109 Safari/537.36 CrKey/1.54.248666",
@@ -31,13 +32,12 @@ class Scraper:
             "Accept-Language": "en-US,en;q=0.8",
             "Connection": "keep-alive",
         }
-        self.car_make = car_make
         self.website = "https://www.otomoto.pl/osobowe"
 
     def scrape_pages(self, number_of_pages: int) -> List[Car]:
         cars = []
         for i in range(1, number_of_pages + 1):
-            current_website = f"{self.website}/{self.car_make}/?page={i}"
+            current_website = f"{self.website}?page={i}"
             new_cars = self.scrape_cars_from_current_page(current_website)
             if new_cars:
                 cars.extend(new_cars)
@@ -54,23 +54,45 @@ class Scraper:
             return []
 
     def extract_cars_from_page(self, soup: BeautifulSoup):
-        offers_table = soup.find("div", class_="ooa-r53y0q ezh3mkl11")
-        cars = offers_table.find_all("article")
-        links = []
-        for car in cars:
-            link = (car.find("h1", class_="e1oqyyyi9 ooa-1ed90th er34gjf0").find("a", href=True).get("href"))
-            links.append(link)
-        for link in links:
-            offer_details = scrap_single_offer(link)
-
-        return offer_details
+        try:
+            offers_table = soup.find("div", class_="ooa-r53y0q ezh3mkl11")
+            cars = offers_table.find_all("article", class_="ooa-yca59n")
+            links = []
+            list_of_cars = []
+            for car in cars:
+                link = car.find("h1", class_="e1oqyyyi9 ooa-1ed90th er34gjf0").find('a').get('href')
+                links.append(link)
+            for link in links:
+                offer_details = self.scrap_single_offer(link)
+                list_of_cars.append(offer_details)
+            return list_of_cars
+        except Exception as e:
+            print(f"Problem with scraping cars, reason: {e}")
+            return []
 
     def scrap_single_offer(self, link):
-        response = requests.get(link, headers=self.headers).text
-        soup = BeautifulSoup(response, "html.parser")
-        table_details = soup.find('div', class_='ooa-1gtr7l5 e18eslyg2')
+        try:
+            response = requests.get(link, headers=self.headers).text
+            soup = BeautifulSoup(response, "html.parser")
+            divs = soup.findAll('div',  {"data-testid": "advert-details-item"})
+            one_car_details = []
+            for div in divs:
+                dic = {}
+                item = div.find('p', class_='e18eslyg4').get_text(strip=True)
+                if div.find('a') is not None:
+                    link = div.find('a').get_text(strip=True)
+                    dic[item] = link
+                if div.find('p', class_='e16lfxpc0') is not None:
+                    paragraph = div.find('p', class_='e16lfxpc0').get_text(strip=True)
+                    dic[item] = paragraph
+                one_car_details.append(dic)
+            return one_car_details
+        except Exception as e:
+            print(f"Problem with scraping website, reason: {e}")
+            return []
 
-        brand = table_details.find('a', class_='ooa-162vy3d e18eslyg3').text
-        full_name = table_details.find('a', class_='ooa-162vy3d e18eslyg3').text
 
-
+sc = Scraper()
+lista = sc.scrape_pages(1)
+for i in lista[:3]:
+    print(i)
